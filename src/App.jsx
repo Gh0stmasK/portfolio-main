52be5965c1d87799e9aa868c1a1ab9fddc64ea7a
Gh0stmasK <soura20v25@gmail.com>
2026-02-04 17:15:06 +0530

    Works section done

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,21 @@ import ServiceSummary from "./sections/ServiceSummary";
 import Services from "./sections/Services";
 import ReactLenis from "lenis/react";
 import About from "./sections/About";
+import Works from "./sections/Works";
 
 const App = () => {
   return (
     <>
-      <ReactLenis root className="relative w-screen min-h-screen overflow-x-auto">
+      <ReactLenis
+        root
+        className="relative w-screen min-h-screen overflow-x-auto"
+      >
         <Navbar />
         <Hero />
         <ServiceSummary />
         <Services />
         <About />
+        <Works />
       </ReactLenis>
     </>
   );
